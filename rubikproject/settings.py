c9117715764d70e8e006eafa735621aa05b2a01d
rubikwebsite3-star <rubikwebsite3@gmail.com>
2026-05-06 14:54:45 +0530

    Update settings.py

--- rubikproject/settings.py
+++ rubikproject/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
 
 # DATABASES = {
 #     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL')
+#         default=os.environ.get('DATABASE_PUBLIC_URL')
 #     )
 # }
 
